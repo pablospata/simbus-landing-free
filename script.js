616edbe3220388b2112ea797fb00a25f8165b7bc
pablospata <pablospata@hotmail.com>
2023-08-22 12:09:27 -0300

    Agregar secciones y corregir responsive

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ for (var i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+function openModal(imgSrc) {
+  const modal = document.getElementById('imageModal');
+  const modalImg = document.getElementById('img01');
+
+  modalImg.src = imgSrc;
+  modal.style.display = "flex";
+  setTimeout(() => {
+      modal.style.opacity = "1";
+  }, 10);
+}
+
+function closeModal() {
+  const modal = document.getElementById('imageModal');
+  modal.style.opacity = "0";
+  setTimeout(() => {
+      modal.style.display = "none";
+  }, 300);
+}
+
+document.querySelectorAll('.project-container img').forEach(img => {
+  img.addEventListener('click', function() {
+      openModal(this.src);
+  });
+});
